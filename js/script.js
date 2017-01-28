943b16aeea695324aeb33083eaedbc9a9a45b18d
Danny Kim <dkim7474@gmail.com>
2017-01-28 18:42:41 -0500

    project container style fix

--- js/script.js
+++ js/script.js
@@ -4,18 +4,13 @@ $(document).ready(function(){
 	// .overlay div opacity is set to 0 default
 	// when you mouse over the .overlay div
 		$(this).animate({opacity: 0.9});
-		// the moused over overlay div will animate the 
+		// the moused over overlay div will animate the
 		// opacity to 1
 		$(this).mouseleave(function(){
 		// when the mouse leaves the overlay div
 			$(this).animate({opacity: 0});
 			// the overlay div's opacity will be set to 0
 		})
-	});
-	// jQuery .animate method does not allow color animations
-	// unless a plugin is used.
-	$(".fa-envelope-o").on("click", function(){
-		$("#hidden-email").slideToggle(700);
 	});
 	$("#project-link").on("click", function(){
 		$.smoothScroll({
@@ -37,4 +32,11 @@ $(document).ready(function(){
 		});
 		return false;
 	});
+
+	var projectContainer = document.getElementById('project-container');
+	projectContainer.addEventListener('mouseover', function(e) {
+		console.log(e.target.nodeName);
+	});
+
+
 });
